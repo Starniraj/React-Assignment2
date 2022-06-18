70399cb83a86e397354d65a684006f67e5acfd7b
Niraj Singh <102036558+Starniraj@users.noreply.github.com>
2022-06-18 12:34:42 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import "./components/handSon2/style.css";
+import FormDetail from './components/handSon2/formDetails';
+function App() {
+  return (
+    <div className="App">
+       <h1><u>Employee Feedback Form</u></h1>
+      <FormDetail/>
+    </div>
+  );
+}
+
+export default App;
